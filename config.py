42dcb6e141bb3dc4fddd30ce92a24bf94ca57906
LeoFernandez <nizares97@gmail.com>
2024-10-15 03:39:09 -0300

    Fix Config

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 class Config:
     # Configuración para conectarse a la base de datos PostgreSQL
     HOST = 'localhost'
-    PORT = 3306
-    USER = 'root'
-    PASSWORD = 'admin'
+    PORT = 5432
+    USER = 'postgres'
+    PASSWORD = 'postgres'
     DB = 'transportador'
     SECRET_KEY = 'admin'
-    USER_DB = ''
-    PASS_DB = ''
-    URL_DB = 'localhost'
-    NAME_DB = 'transportador'
-    FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
+    FULL_URL_DB = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
