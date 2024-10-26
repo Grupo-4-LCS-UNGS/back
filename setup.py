10dd61a1316136dabd36bb38fccfb7e25de0ce9a
Drago177 <facundorios26@gmail.com>
2024-10-25 22:22:40 -0300

    Creación de tablas

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #from main import db
+from sys import exception
 
 from psycopg2 import connect, OperationalError, errorcodes, extensions
 
@@ -12,28 +13,48 @@ from config import Config
 # from models.vehiculo import Vehiculo
 
 def crear_base():
-    base = connect(host= Config.HOST,
-                   port= Config.PORT,
-                   user= Config.USER,
-                   password= Config.PASSWORD,
-                   database= 'postgres')#Config.DB)
-    
-    base.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    base = abrir_conexion('postgres')
 
     cur = base.cursor()
 
     try:
+        cur.execute(f'DROP DATABASE IF EXISTS {Config.DB}')
         cur.execute(f'CREATE DATABASE {Config.DB}')
 
     except OperationalError as err:
         if err.pgerror == errorcodes.DUPLICATE_DATABASE:
             pass
-    
+
     cur.close()
 
     base.close()
 
+def crear_tablas():
+    base = abrir_conexion(Config.DB)
+    cur = base.cursor()
+
+    with open('tablas.sql', 'r') as file:
+        sql = file.read()
+    try:
+        cur.execute(sql)
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        base.close()
+
+def abrir_conexion(db_nombre):
+    base = connect(host=Config.HOST,
+                   port=Config.PORT,
+                   user=Config.USER,
+                   password=Config.PASSWORD,
+                   database=db_nombre)  # Config.DB)
+    base.set_isolation_level(extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    return base
+
+
 if __name__ == '__main__':
     crear_base() #esto instancia crea la base de datos
+    crear_tablas() #esto crea las tablas
     #db.drop_all() #esto borra las tablas
     #db.create_all() #esto las crea nuevamente
